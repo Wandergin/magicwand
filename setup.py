996e97e1792d8e04ab7e0ac2ffc36310c92b2c6c
Wandergin <9088018+Wandergin@users.noreply.github.com>
2022-05-26 15:11:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="magicwand",
     version="1.0.0",
     author="Alexander Reynolds",
-    install_requires=["opencv-contrib-python"],
+    install_requires=["opencv-python"],
     packages=find_packages(),
     description="Flood filling masking tool.",
 )
